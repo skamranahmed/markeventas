820275e9203f4b7c5d7eb7a1172988964f8994a8
skahmed <skahmed@myamu.ac.in>
2022-01-27 22:00:02 +0530

    add logger

--- go.mod
+++ go.mod
@@ -2,12 +2,18 @@ module github.com/skamranahmed/twitter-create-gcal-event-api
 
 go 1.17
 
-require github.com/spf13/viper v1.10.1
+require (
+	github.com/golang/glog v1.0.0
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
+	github.com/spf13/viper v1.10.1
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
---
+++ pkg/log/logger.go
@@ -0,0 +1,67 @@
+package log
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/golang/glog"
+	"github.com/mgutz/ansi"
+)
+
+// CallingDepth : Depth of stack to go back to get proper line and file function called from
+const (
+	CallingDepth = 1
+
+	// Background colors to use
+	ErrorColor   = "red"
+	WarningColor = "yellow"
+	InfoColor    = "green"
+	FatalColor   = "magenta+b"
+
+	// Base error string
+	BaseString = ` * `
+)
+
+func init() {
+	flag.Parse()
+	flag.Lookup("alsologtostderr").Value.Set("true")
+}
+
+// Info : Wrapper around glog Info
+func Info(args ...interface{}) {
+	glog.InfoDepth(CallingDepth, getColorString(InfoColor), args)
+}
+
+// Info : Wrapper around glog Info
+func Infof(formatString string, args ...interface{}) {
+	msg := fmt.Sprintf(formatString, args...)
+	glog.InfoDepth(CallingDepth, getColorString(InfoColor), msg)
+}
+
+// Warning : Wrapper around glog Warning
+func Warning(args ...interface{}) {
+	glog.WarningDepth(CallingDepth, getColorString(WarningColor), args)
+}
+
+// Error : Wrapper around glog Error
+func Error(args ...interface{}) {
+	colorString := getColorString(ErrorColor)
+	glog.ErrorDepth(CallingDepth, colorString, args)
+}
+
+// Errorf : Wrapper around glog Error
+func Errorf(formatString string, args ...interface{}) {
+	msg := fmt.Sprintf(formatString, args...)
+	glog.ErrorDepth(CallingDepth, getColorString(ErrorColor), msg)
+}
+
+// Fatal : Wrapper around glog Fatal
+func Fatal(args ...interface{}) {
+	glog.FatalDepth(CallingDepth, getColorString(FatalColor), args)
+}
+
+func getColorString(backgroundColor string) string {
+	baseStr := ` * `
+	str := fmt.Sprintf(`|%s| `, ansi.Color(baseStr, "white:"+backgroundColor))
+	return str
+}
