d2ce881d8c62869848af010eca04ce89e27a9bb3
skahmed <skahmed@myamu.ac.in>
2022-02-21 17:38:09 +0530

    Add db column in users table to maintain the status of google oauth token

--- internal/models/user.go
+++ internal/models/user.go
@@ -7,8 +7,9 @@ import (
 // User : represents the schema of `users` table
 type User struct {
 	gorm.Model
-	TwitterID         string `gorm:"uniqueIndex;not null" json:"twitter_id"`
-	TwitterScreenName string `gorm:"not null" json:"twitter_screen_name"`
+	TwitterID              string `gorm:"uniqueIndex;not null" json:"twitter_id"`
+	TwitterScreenName      string `gorm:"not null" json:"twitter_screen_name"`
+	IsGcalOauthTokenActive bool   `gorm:"not null;default:false" json:"is_gcal_oauth_token_active"` // to maintain the status of google calendar oauth token
 
 	// has one relationship
 	GoogleCalendarToken Token
